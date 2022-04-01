d641ea9c84436a16a4595b38b93bdfe6aaf3ceef
Olivier B. OURA <baudoliver7@gmail.com>
2022-04-01 15:24:17 +0000

    fix: set payment_deadline value (#29)
    While registering a new user.
    
    Fix: #27

---
+++ src/main/java/io/surati/gap/payment/base/db/PaymentBaseDatabaseBuiltWithLiquibase.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2022 Surati
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to read
+ * the Software only. Permissions is hereby NOT GRANTED to use, copy, modify,
+ * merge, publish, distribute, sublicense, and/or sell copies of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package io.surati.gap.payment.base.db;
+
+import com.baudoliver7.easy.liquibase4j.gen.UncheckedLiquibaseDataSource;
+import com.baudoliver7.jdbc.toolset.wrapper.DataSourceWrap;
+import javax.sql.DataSource;
+
+/**
+ * Data source decorator that automatically build Payment module database with liquibase.
+ *
+ * @since 0.1
+ */
+public final class PaymentBaseDatabaseBuiltWithLiquibase extends DataSourceWrap {
+
+    /**
+     * Changelog master file name.
+     */
+    public static final String CHANGELOG_MASTER_FILENAME =
+        "io/surati/gap/payment/base/liquibase/db.postgresql.changelog-master.xml";
+
+    /**
+     * Ctor.
+     * @param src Data source
+     */
+    public PaymentBaseDatabaseBuiltWithLiquibase(final DataSource src) {
+        super(
+            new UncheckedLiquibaseDataSource(
+                src, PaymentBaseDatabaseBuiltWithLiquibase.CHANGELOG_MASTER_FILENAME
+            )
+        );
+    }
+}
